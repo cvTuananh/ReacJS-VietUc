bab9abefd0f540159c5d9303ccbca1c0b067b566
TuanAnhViCu <anh.cu@madison-technogoly.com>
2021-12-08 21:01:56 +0700

    lab04

--- lab04/src/componet/SimplePhoto.js
+++ lab04/src/componet/SimplePhoto.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import data from '../componet/asset/data.json'
-import '../componet/SimplePhoto.css'
-const app = document.getElementById('app');
-
-
+import React from "react";
+import data from "../componet/asset/data.json";
+import "../componet/SimplePhoto.css";
+const app = document.getElementById("app");
 
 class App extends React.Component {
 	render() {
-		return (
-			<Tiles data={this.props.data} />
-		);
+		return <Tiles data={this.props.data} />;
 	}
 }
 
 class Tiles extends React.Component {
 	render() {
-		
 		return (
 			<div className="tiles">
 				{this.props.data.map((data) => {
-					return <Tile data={data} key={data.id} />
+					return <Tile data={data} key={data.id} />;
 				})}
 			</div>
 		);
@@ -28,70 +23,69 @@ class Tiles extends React.Component {
 
 class Tile extends React.Component {
 	constructor(props) {
-			super(props);
-			this.state = {
-				open: false,
-				mouseOver: false
-			};
-			// Bind properties to class instance
-			this._clickHandler = this._clickHandler.bind(this);
-			this._mouseEnter = this._mouseEnter.bind(this);
-			this._mouseLeave = this._mouseLeave.bind(this);
-		}
-		// Event handlers to modify state values
+		super(props);
+		this.state = {
+			open: false,
+			mouseOver: false,
+		};
+
+		this._clickHandler = this._clickHandler.bind(this);
+		this._mouseEnter = this._mouseEnter.bind(this);
+		this._mouseLeave = this._mouseLeave.bind(this);
+	}
+
 	_mouseEnter(e) {
 		e.preventDefault();
 		if (this.state.mouseOver === false) {
 			console.log(this.props.data.name);
 			this.setState({
-				mouseOver: true
-			})
+				mouseOver: true,
+			});
 		}
 	}
 	_mouseLeave(e) {
 		e.preventDefault();
 		if (this.state.mouseOver === true) {
 			this.setState({
-				mouseOver: false
-			})
+				mouseOver: false,
+			});
 		}
 	}
 	_clickHandler(e) {
 		e.preventDefault();
 		if (this.state.open === false) {
 			this.setState({
-				open: true
+				open: true,
 			});
 		} else {
 			this.setState({
-				open: false
+				open: false,
 			});
 		}
 	}
 
 	render() {
-		// Modify styles based on state values
 		let tileStyle = {};
 		let headerStyle = {};
 		let zoom = {};
-		// When tile clicked
+
 		if (this.state.open) {
 			tileStyle = {
-				width: '62vw',
-				height: '62vw',
-				position: 'absolute',
-				top: '50%',
-				left: '50%',
-				margin: '0',
-				marginTop: '-31vw',
-				marginLeft: '-31vw',
-				boxShadow: '0 0 40px 5px rgba(0, 0, 0, 0.3)',
-				transform: 'none'
+				width: "62vw",
+				height: "62vw",
+				position: "absolute",
+				top: "50%",
+				left: "50%",
+				margin: "0",
+				marginTop: "-31vw",
+				marginLeft: "-31vw",
+				boxShadow: "0 0 40px 5px rgba(0, 0, 0, 0.3)",
+				transform: "none",
 			};
 		} else {
 			tileStyle = {
-				width: '18vw',
-				height: '18vw'
+				width: "18vw",
+				height: "18vw",
 			};
 		}
 
@@ -110,4 +104,4 @@ class Tile extends React.Component {
 	}
 }
 
-export default App
+export default App;
